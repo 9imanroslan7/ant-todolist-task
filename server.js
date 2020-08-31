0d6c6f8416489123ba85d8fdb225c92a4644b3e2
iman roslan <imanbiz.work@gmail.com>
2020-08-31 16:29:45 +0800

    change dir back to /dist/angular-testing

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// app.use(express.static(__dirname + '/dist/angular-testing'));
-// app.get('/*', function(req,res) {
-// res.sendFile(path.join(__dirname+
-// '/dist/angular-testing/index.html'));});
-// app.listen(process.env.PORT || 8080);
-
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/angular-testing'));
+app.use(express.static(__dirname + '/dist/angular-testing'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/angular-testing/src/index.html'));});
+'/dist/angular-testing/index.html'));});
 app.listen(process.env.PORT || 8080);
+
+
