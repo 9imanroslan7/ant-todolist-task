0dac3489b2ce02d9f2a220ae9c69e23ccd94f5d6
iman roslan <imanbiz.work@gmail.com>
2020-08-31 16:18:27 +0800

    change directory

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static(__dirname + '/dist/angular-testing'));
+// app.get('/*', function(req,res) {
+// res.sendFile(path.join(__dirname+
+// '/dist/angular-testing/index.html'));});
+// app.listen(process.env.PORT || 8080);
+
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-testing'));
+app.use(express.static(__dirname + '/angular-testing'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/angular-testing/index.html'));});
+'/angular-testing/src/index.html'));});
 app.listen(process.env.PORT || 8080);
