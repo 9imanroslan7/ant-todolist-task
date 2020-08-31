0ba5ef045c19e3c73ac02a1749aa98504ae43755
iman roslan <imanbiz.work@gmail.com>
2020-08-31 17:07:30 +0800

    change app-name

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/angular-testing'));
+app.use(express.static(__dirname + '/dist/angularTesting'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/angular-testing/index.html'));});
+'/dist/angularTesting/index.html'));});
 app.listen(process.env.PORT || 8080);
 
 
